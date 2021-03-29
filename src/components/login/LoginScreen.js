5436d8277e2957d18874f329cd8ef4022b62cb2c
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-03-29 02:07:30 +0200

    login invitado

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,66 @@
 import React, { useContext } from 'react';
+import { Container } from 'react-bootstrap';
+import styled from '@emotion/styled';
+
 import { AuthContext } from '../../auth/AuthContext';
 import { useForm } from '../../hooks/useForm';
 import { types } from '../../types/types';
+
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: var(--lighter);
+
+  #login-form{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    background-color: var(--primary);
+    width: 30rem;
+    height: 20rem;
+    padding: 2rem;
+  }
+
+  #login{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  #login label{
+    margin: 1rem auto;
+    color: var(--baseWhite);
+    font-weight: bold;
+
+  }
+
+  #login small{
+    align-self: flex-start;
+    margin: 1rem;
+    color: var(--baseWhite);
+  }
+
+  #login-form input{
+    border-radius: 2rem;
+    margin: 0 1rem;
+    padding: .5rem 1rem;
+    color: var(--baseDark);
+    font-size: 1.25rem;
+  }
+
+  #login-form button{
+    background-color: var(--secondary);
+    font-weight: bold;
+    color: var(--lighter);
+    margin: 0 auto;
+    padding: 0.5rem 1rem;
+    font-size: 1.5rem;
+  }
+`;
 
 export const LoginScreen = ({history}) => {
 
@@ -26,26 +85,34 @@ export const LoginScreen = ({history}) => {
     });
     history.replace(lastPath)
   }
-
+  
   return (
-    <div>
-      <form
-        onClick={handleLogin}
-      >
-        <input
-          type='text'
-          className='form-control'
-          name='name'
-          defaultValue='Invitado'
-          onChange={handleInputChange}
-          disabled
-        />
-        <button
-          type='submit'
-          className='btn btn-primary'
-          onClick={handleLogin}
-        >Login</button>
-      </form>
-    </div>
+    <Container fluid='md'>
+      <Login>
+        <form onClick={handleLogin} id='login-form'>
+          <div id='login' className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              className='form-control'
+              name='name'
+              defaultValue='Invitado'
+              aria-describedby="nameHelp"
+              onChange={handleInputChange}
+              disabled
+            />
+            <small 
+              id='nameHelp'
+              className='form-text texte-muted'
+            >Welcome to Daitool!</small>
+          </div>
+          <button
+            type='submit'
+            className='btn'
+            onClick={handleLogin}
+          >Login</button>
+        </form>
+      </Login>
+    </Container>
   )
 }
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import {BrowserRouter as Router, Switch} from 'react-router-dom';
-import { AuthContext } from '../auth/AuthContext';
+import {Global, css} from '@emotion/react';
 
+import { AuthContext } from '../auth/AuthContext';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 import { PrivateRoute } from './PrivateRoute';
@@ -12,21 +13,199 @@ export const AppRouter = () => {
   const {user} = useContext(AuthContext);
 
   return (
-    <Router>
-      <div>
-        <Switch>
-          <PublicRoute 
-            exact path='/login' 
-            component={LoginScreen}
-            isAuthenticated={user.logged}
-          />
-          <PrivateRoute 
-            path='/' 
-            component={DashboardRoutes}
-            isAuthenticated={user.logged}
+    <Fragment>
+      <Global
+        styles={css`
+          :root{
+
+            // Primary colours
+            --primary: #FF723D;
+            --secondary: #92D2D9;
+
+            // Light colours
+            --light: #ECECEC;
+            --lighter: #F9F9FB;
+
+            // Base colours
+            --baseDark: #232323;
+            --baseWhite: white; 
+
+            // Additional colours 
+            --warningError: #FB5341;
+            --links: #FF723D;
+            --inBodyLinks: #FF723D
+
+          }
+
+          html {
+            font-size: 62.5%; // 10px = 1rem
+          }
+
+          body{
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5rem;
+            font-family: 'Mulish', sans-serif;
+          }
+
+          h1{
+            font-weight: bold;
+            font-size: 6.4rem;
+            line-height: 8.6rem;
+            letter-spacing: 0.02rem;
+          }
+
+          h2{
+            font-weight: 400;
+            font-size: 4.6rem;
+            line-height: 6.2rem;
+            letter-spacing: 0rem;
+          }
+
+          h3{
+            font-weight: bold;
+            font-size: 3rem;
+            line-height: 4.6rem;
+            letter-spacing: 0.01rem;
+          }
+
+          h4{
+            font-weight: bold;
+            font-size: 2.4rem;
+            line-height: 3.2rem;
+            letter-spacing: 0.02rem;
+          }
+
+          h4 .regular{
+            font-weight: 400;
+            font-size: 1.8rem;
+            line-height: 2.4rem;
+            letter-spacing: 0.01rem;
+          }
+
+          h5{
+            font-weight: bold;
+            font-size: 6.4rem;
+            line-height: 8.6rem;
+            letter-spacing: 0.02rem;
+          }
+
+          h6{
+            font-weight: 600;
+            font-size: 2.4rem;
+            line-height: 3rem;
+            letter-spacing: 0.01rem;
+          }
+
+          h6 .selected{
+            font-weight: bold;
+            font-size: 2.4rem;
+            line-height: 3rem;
+            letter-spacing: 0.01rem;
+          }
+
+          .copy{
+            font-weight: 400;
+            font-size: 1.8rem;
+            line-height: 3.4rem;
+            letter-spacing: 0.02rem;
+          }
+
+          .inCopy .header {
+            font-weight: bold;
+            font-size: 2.4rem;
+            line-height: 3rem;
+            letter-spacing: 0.02rem;
+          }
+
+          nav{
+            font-weight: 600;
+            font-size: 1.6rem;
+            line-height: 2.4rem;
+            letter-spacing: 0.02rem
+          }
+
+          .breadcrumbs{
+            font-weight: 400;
+            font-size: 1.5rem;
+            line-height: 2rem;
+            letter-spacing: 0.02rem
+          }
+
+          .button{
+            font-weight: 600;
+            font-size: 1.6rem;
+            line-height: 2.4rem;
+            letter-spacing: 0.02rem
+          }
+
+          .selectedProducts{
+            font-weight: bold;
+            font-size: 2rem;
+            line-height: 2.8rem;
+            letter-spacing: 0.02rem
+          }
+
+          .selectedProducts .smallerPrice{
+            font-weight: 400;
+            font-size: 2.4rem;
+            line-height: 8.6rem;
+            letter-spacing: 0.02rem
+          }
+
+          .footer .header{
+            font-weight: bold;
+            font-size: 1.8rem;
+            line-height: 2.4rem;
+            letter-spacing: 0.02rem
+          }
+          
+          .footer .links{
+            font-weight: 400;
+            font-size: 1.6rem;
+            line-height: 3.6rem;
+            letter-spacing: 0.02rem
+          }
+
+          .clientArea .header{
+            font-weight: bold;
+            font-size: 1.6rem;
+            line-height: 2.2rem;
+            letter-spacing: 0.02rem
+          }
+
+          .clientArea .text{
+            font-weight: 400;
+            font-size: 1.6rem;
+            line-height: 3.2rem;
+            letter-spacing: 0.02rem
+          }
+          
+          .clientArea .link{
+            font-weight: 600;
+            font-size: 1.4rem;
+            line-height: 2.2rem;
+            letter-spacing: 0.02rem
+          }
+          
+        `}
+      />
+      <Router>
+        <div>
+          <Switch>
+            <PublicRoute 
+              exact path='/login' 
+              component={LoginScreen}
+              isAuthenticated={user.logged}
             />
-        </Switch>
-      </div>
-    </Router>
+            <PrivateRoute 
+              path='/' 
+              component={DashboardRoutes}
+              isAuthenticated={user.logged}
+              />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
   )
 }
